1dbeba4c3dc0bb0233ddee8eb947e4d7301119be
poopy2121 <ggg1111g1g1g@gmail.com>
2024-11-04 10:17:33 +0100

    was geht

---
+++ script.js
@@ -0,0 +1,24 @@
+const foods = [
+    {
+        name: "chickenbreast",
+        price: 6.99,
+        category: "proteins",
+        protein: 31
+ 
+    },
+
+    {
+        name: "Thunfisch im eigenen Saft",
+        price: 1.39,
+        category: "proteins",
+        protein: 29
+    },
+
+    {
+        name: "Magerquark",
+        price: 1.49,
+        category: "proteins"
+    }
+
+    
+]
